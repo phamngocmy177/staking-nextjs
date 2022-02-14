7771dc8fa1f5c7cc02fca822a0e252bf480ec318
ngocmy <phamngocmy177@gmail.com>
2022-02-14 21:32:33 +0700

    add classify token

--- ethereum/hooks/useUserTokens.js
+++ ethereum/hooks/useUserTokens.js
@@ -1,6 +1,21 @@
 import { useMoralis } from "react-moralis";
 import useSWR from "swr";
 import { useActiveWeb3React } from "..//hooks/web3";
+import { TOKEN_CLASSES } from "../constants/tokens";
+import { isUniswapLP, isSushiswapLP } from "../hooks/tokenClassification";
+
+const classifyTokens = (token) => {
+  if (isUniswapLP(token) || isSushiswapLP(token)) {
+    return {
+      ...token,
+      class: TOKEN_CLASSES.LP_TOKEN,
+    };
+  }
+  return {
+    ...token,
+    class: TOKEN_CLASSES.TOKEN,
+  };
+};
 
 export const useUserTokens = () => {
   const { Moralis } = useMoralis();
@@ -12,6 +27,9 @@ export const useUserTokens = () => {
     await Moralis.Web3API.account.getTokenBalances(options);
   const address = process.env.NEXT_PUBLIC_MORALIS_SERVER_URL;
   const { data } = useSWR(address, fetcher);
-  if (data) return data;
+  if (data) {
+    const classifiedTokens = data?.map((item) => classifyTokens(item));
+    return classifiedTokens;
+  }
   return [];
 };
--- pages/portfolio.js
+++ pages/portfolio.js
@@ -1,12 +1,8 @@
 import Layout from "layouts/Layout";
-import React, { useEffect } from "react";
-import { useMoralis } from "react-moralis";
-import { useActiveWeb3React } from "../ethereum/hooks/web3";
+import React from "react";
 import { useUserTokens } from "../ethereum/hooks/useUserTokens";
 
 function Portfolio() {
-  const { Moralis } = useMoralis();
-  const { account, chainId } = useActiveWeb3React();
   const userTokens = useUserTokens();
 
   return <Layout></Layout>;
